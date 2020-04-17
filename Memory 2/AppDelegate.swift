932ffd8e14075caf8cc2ab64d462b140f83fae04
Ocean1878 <sikefayati@icloud.com>
2020-04-17 12:12:30 +0200

    Initial Commit

---
+++ Memory 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Memory 2
+//
+//  Created by Iman Kefayati on 17.04.20.
+//  Copyright © 2020 Iman Kefayati. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Memory 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Memory 2
+//
+//  Created by Iman Kefayati on 17.04.20.
+//  Copyright © 2020 Iman Kefayati. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
